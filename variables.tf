6baa99be1c644927d676169f7d8c3f02a29501ea
JuanBSoares <juanbsoares@outlook.com>
2025-01-07 14:35:20 -0300

    adding project_name as a variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   type        = string
   description = "Network CIDR Block for VPC"
 }
+
+variable "project_name" {
+  type        = string
+  description = "Project Name to be used to name Resources, as a tag"
+}
