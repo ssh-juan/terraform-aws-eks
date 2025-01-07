1f2da6aa926ffaab0eb2fe534ad7574ee0d9bbbf
JuanBSoares <juanbsoares@outlook.com>
2025-01-07 15:00:33 -0300

    creating internet gateway, route table and route table association

---
+++ igw_and_rtb.tf
@@ -0,0 +1,36 @@
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-igw"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_pub_rtb" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-pub-rtb"
+    }
+  )
+}
+
+resource "aws_route_table_association" "eks_rtb_asc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_pub_rtb.id
+}
+
+resource "aws_route_table_association" "eks_rtb_asc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_pub_rtb.id
+}
