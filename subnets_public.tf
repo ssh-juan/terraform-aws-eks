403088b22c6e60fc244503a52657f7a5f008f351
JuanBSoares <juanbsoares@outlook.com>
2025-01-07 14:34:52 -0300

    creating separate files for subnets and adding project_name as a variable

--- subnets_public.tf
+++ subnets_public.tf
@@ -7,7 +7,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
   tags = merge(
     local.tags,
     {
-      Name                     = "eks_subnet_public_1a",
+      Name                     = "${var.project_name}-subnet-public-1a",
       "kubernetes.io/role/elb" = 1
     }
   )
@@ -22,7 +22,7 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   tags = merge(
     local.tags,
     {
-      Name                     = "eks_subnet_public_1b",
+      Name                     = "${var.project_name}-subnet-public-1b",
       "kubernetes.io/role/elb" = 1
     }
   )
