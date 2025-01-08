b530d4a75b46cbc800a0ab3f527a37a34f55d7a7
JuanBSoares <juanbsoares@outlook.com>
2025-01-08 14:40:38 -0300

    adding managed node group module

--- modules.tf
+++ modules.tf
@@ -12,3 +12,12 @@ module "eks_cluster" {
   subnet_pub_1a = module.eks_network.subnet_pub_1a
   subnet_pub_1b = module.eks_network.subnet_pub_1b
 }
+
+module "eks_managed_node_group" {
+  source         = "./modules/managed-node-group"
+  project_name   = var.project_name
+  cluster_name   = module.eks_cluster.cluster_name
+  subnet_priv_1a = module.eks_network.subnet_priv_1a
+  subnet_priv_1b = module.eks_network.subnet_priv_1b
+  tags           = local.tags
+}
--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -5,3 +5,7 @@ output "eks_vpc_config" {
 output "oidc" {
   value = data.tls_certificate.eks_oidc_tls_certificate.certificates[*].sha1_fingerprint
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.id
+}
