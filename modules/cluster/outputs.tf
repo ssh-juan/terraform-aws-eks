1289ef518a4ac1f5d8d211f5cdc7cd1d912b0b80
JuanBSoares <juanbsoares@outlook.com>
2025-01-09 00:08:19 -0300

    setting up the iam policy for the controller

--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -9,3 +9,7 @@ output "oidc" {
 output "cluster_name" {
   value = aws_eks_cluster.eks_cluster.id
 }
+
+output "oidc_id" {
+  value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "eks_vpc_config" {
 output "oidc" {
   value = module.eks_cluster.oidc
 }
+
+output "oidc_id" {
+  value = module.eks_cluster.oidc_id
+}
